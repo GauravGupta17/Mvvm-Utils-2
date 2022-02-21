5404aae1b046e57944520650e2d8492fa50c44de
admin <gauravkld007>
2022-02-21 21:39:45 +0530

    Fixing Splitties

--- mvvm-utils/src/main/java/com/parent/mvvm_utils/utils/listerner/ConnectivityHelper.kt
+++ mvvm-utils/src/main/java/com/parent/mvvm_utils/utils/listerner/ConnectivityHelper.kt
@@ -2,6 +2,7 @@ package com.parent.mvvm_utils.utils.listerner
 
 import splitties.systemservices.connectivityManager
 
+
 fun isConnected(): Boolean {
     val networkInfo = connectivityManager.activeNetworkInfo
     return networkInfo?.isConnected ?: false
